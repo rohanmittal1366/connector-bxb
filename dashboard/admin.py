57b5c0cae78ade0346163d36b398284ee2a4d2a1
rohan_mittal <rohanmittal1366@gmail.com>
2021-10-21 21:59:49 +0530

    added database for Product

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+# declare here and it will access the product and show it to you on admin panel
+admin.site.register(Product)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY = (
+    ('Stationary', 'Stationary'),
+    ('Electronics', 'Electronics'),
+    ('Food', 'Food'),
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100 , null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY , null=True)
+    quantity = models.PositiveIntegerField(null=True)
+ 
+    # It shows how the thinkg gonna show on admin panel 
+    # This return itself value like Milk-100 on admin panel
+    def __str__(self):
+        return f'{self.name}-{self.quantity}'
