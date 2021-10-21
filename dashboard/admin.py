de44eaf0a863a5557506f0e3bd6d958a041b62ef
rohan_mittal <rohanmittal1366@gmail.com>
2021-10-21 22:17:06 +0530

    customize admin panel

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Product
-
+from django.contrib.auth.models import Group
 # Register your models here.
+
+admin.site.site_header = 'Connector BxB'
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name' ,'category', 'quantity')
+    list_filter = ['category'] # it filter the content by category in admin panel
+
+
 # declare here and it will access the product and show it to you on admin panel
-admin.site.register(Product)
+admin.site.register(Product , ProductAdmin)
+# admin.site.unregister(Group)
