5647ef994f384a91799ce8239fca6c7e98cd360d
Pradumna <pradumnabajoria@gmail.com>
2021-10-23 21:41:40 +0530

    refactored code

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,19 +5,19 @@ from .models import Product
 
 # Create your views here.
 
-@login_required
+@login_required(login_url='user-login')
 def index(request):
     return render(request, "dashboard/index.html")
     # return HttpResponse('This is index page')
 
-@login_required
+@login_required(login_url='user-login')
 def staff(request):
     return render(request, "dashboard/staff.html")
 
-@login_required
+@login_required(login_url='user-login')
 def product(request):
     return render(request, "dashboard/product.html")
 
-@login_required
+@login_required(login_url='user-login')
 def order(request):
     return render(request, "dashboard/order.html")
