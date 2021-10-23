2536c9b28ea08b26fc3fb5853ffe7d944da9a3b7
Kishan2029 <kishanprajapati2029@gmail.com>
2021-10-23 13:15:19 +0530

    Modified userCreationForm; Added forms.py

--- user/views.py
+++ user/views.py
@@ -1,21 +1,24 @@
 from django import forms
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+
+
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('dashboard-index')
     else:
-        form = UserCreationForm()
-    form = UserCreationForm()
+        form = CreateUserForm()
+  
     context = {
         'form' : form,
 
     } 
-    
+
     return render(request, 'user/register.html', {'form': form})
