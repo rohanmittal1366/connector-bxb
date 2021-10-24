314017b9e4c7dfab476f3d617ed036839f761783
rohan_mittal <rohanmittal1366@gmail.com>
2021-10-25 00:41:09 +0530

    user can request for order

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.db import models
 from django.db.models import fields
-from .models import Product
+from .models import Product , Order
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name' , 'category' , 'quantity']
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['product', 'order_quantity'] 
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,14 +2,29 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required # for give permision according to the user and admin
 from .models import Product, Order
-from .forms import ProductForm
+from .forms import ProductForm, OrderForm
 from django.contrib.auth.models import User # for give permision according to the user and admin
 
 # Create your views here.
 
 @login_required(login_url='user-login')
 def index(request):
-    return render(request, "dashboard/index.html")
+    orders = Order.objects.all()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            # we need an instance bcz we want to save user name for making request
+            instance =  form.save(commit=False)
+            instance.staff = request.user
+            instance.save()
+            return redirect('dashboard-index')
+    else:
+        form = OrderForm()
+    context = {
+        'orders' : orders,
+        'form' : form,
+    } 
+    return render(request, "dashboard/index.html" , context)
     # return HttpResponse('This is index page')
 
 @login_required(login_url='user-login')
